7385d11342783af3fe6e31a4c5ff998314292bbc
g12918ka <g12918ka@s30446.tsuda.ac.jp>
2014-05-14 16:05:35 +0900

    ドライバー　松井香奈 g12929 ナビゲーター　河内彩花　g12918
    999まで表示できるようにしました。

--- IntToEng/src/IntToEng.java
+++ IntToEng/src/IntToEng.java
@@ -14,26 +14,55 @@ public class IntToEng {
     static String translateEng(int n) {
    String eng="";
    String eng10 = "";
-   if(n>=90) eng10 = "ninty";
+   String eng100 ="";
+   int m = n;
+   if(n>=100){
+	   int l = n/100;
+	   eng100 = countnumber(l);
+	   eng100= eng100+ " hundred";
+	   n = n%100;
+   }
+  
+   if(n==0) eng="zero";
+   else if(n==10) eng="ten";
+	else if(n==11) eng="eleven";
+	else if(n==12) eng="twelve";
+	else if(n==13) eng="thirteen";
+	else if(n==14) eng="fourteen";
+	else if(n==15) eng="fifteen";
+	else if(n==16) eng="sixteen";
+	else if(n==17) eng="seventeen";
+	else if(n==18) eng="egihteen";
+	else if(n==19) eng="ninteen";
+   if(n<10 || 20<=n) {
+	   if(100>n && n>=90) eng10 = "ninety";
    else    if(n>=80) eng10 = "eighty";
    else    if(n>=70) eng10 = "seventy"; 
    else    if(n>=60) eng10 = "sixty"; 
    else    if(n>=50) eng10 = "fifty";
-   else    if(n>=40) eng10 = "fory";
+   else    if(n>=40) eng10 = "forty";
    else    if(n>=30) eng10 = "thrity";
    else    if(n>=20) eng10 = "twenty";
-  n<<=n;
-    	if(n==0) eng="zero";
-    	else if(n==1) eng="one";	
-    	else if(n==2) eng="two";
-    	else if(n==3) eng="three";
-    	else if(n==4) eng="four";
-    	else if(n==5) eng="five";
-    	else if(n==6) eng="six";
-    	else if(n==7) eng="seven";
-    	else if(n==8) eng="eight";
-    	else if(n==9) eng="nine";
-    	String str = Integer.toString(n)+" "+eng10+eng;
+  n=n%10;
+     eng = countnumber(n);
+   }
+    	
+    	String str = Integer.toString(m)+" "+eng100+" "+eng10+" "+eng;
         return str;
     }
+
+	public static String countnumber(int n) {
+		String eng="";
+		if(n==1) eng="one";	
+			else if(n==2) eng="two";
+			else if(n==3) eng="three";
+			else if(n==4) eng="four";
+			else if(n==5) eng="five";
+			else if(n==6) eng="six";
+			else if(n==7) eng="seven";
+			else if(n==8) eng="eight";
+			else if(n==9) eng="nine";
+		return eng;
+	}
 }
+
